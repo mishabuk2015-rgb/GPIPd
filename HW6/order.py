ef597626e5c8dcfbad22d95fd723cc175eac4b90
Your Name <you@example.com>
2026-06-22 12:39:39 +0300

    автопарк

--- HW6/order.py
+++ HW6/order.py
@@ -5,22 +5,22 @@ client_name = input("Введіть ім’я та прізвище: ")
 trip_date = input("Введіть дату поїздки (наприклад, 10.06.2026): ")
 persons = int(input("Введіть кількість осіб: "))
 
-# Вартість за одну особу
+
 price_per_person = 15000
 
-# Загальна вартість
+
 total_price = price_per_person * persons
 
-# Логіка знижки
+
 if persons > 5:
     discount = total_price * 0.05
 else:
     discount = 0
 
-# Фінальна ціна
+
 final_price = total_price - discount
 
-# Формування листа
+
 letter = LETTER_TEMPLATE.format(
     name=client_name,
     date=trip_date,
@@ -31,5 +31,4 @@ letter = LETTER_TEMPLATE.format(
     final_price=int(final_price)
 )
 
-# Вивід результату
 print(letter)
